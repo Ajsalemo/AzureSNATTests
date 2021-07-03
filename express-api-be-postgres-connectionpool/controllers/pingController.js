7ec4661c6a880fb2ffcf34e1007566313093b034
Anthony Salemo <ajssalemo@gmail.com>
2021-07-03 19:52:15 -0400

    updating pool usage

---
+++ express-api-be-postgres-connectionpool/controllers/pingController.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  max: 160,
+  port: 5432,
+});
+
+pool.on("error", (err, _) => {
+  console.error("Error:", err);
+});
+
+const pingController = router.get("/", async (_, res, next) => {
+  const client = await pool.connect();
+  const query = `SELECT * FROM "BMWCarTable";`;
+  try {
+    const { rows } = await client.query(query);
+    client.release();
+    res.json(rows);
+  } catch (error) {
+    console.log("An error has occurred: ", error);
+    next(error);
+  }
+});
+
+module.exports = pingController;
